644d56756533a0799705ff47dee4daacce25653d
brittanyyee <46659636+brittanyyee@users.noreply.github.com>
2020-05-04 12:52:38 -0400

    Update app.js

--- buy_online_pickup_instore/app.js
+++ buy_online_pickup_instore/app.js
@@ -1,123 +1,122 @@
 const mapStyle = [
-  {
-      "featureType": "administrative",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "simplified"
-          },
-          {
-              "hue": "#0066ff"
-          },
-          {
-              "saturation": 74
-          },
-          {
-              "lightness": 100
-          }
-      ]
-  },
-  {
-      "featureType": "poi",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "simplified"
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "simplified"
-          }
-      ]
-  },
-  {
-      "featureType": "road.highway",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          },
-          {
-              "weight": 0.6
-          },
-          {
-              "saturation": -85
-          },
-          {
-              "lightness": 61
-          }
-      ]
-  },
-  {
-      "featureType": "road.highway",
-      "elementType": "geometry",
-      "stylers": [
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "road.arterial",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "road.local",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "simplified"
-          }
-      ]
-  },
-  {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "simplified"
-          },
-          {
-              "color": "#5f94ff"
-          },
-          {
-              "lightness": 26
-          },
-          {
-              "gamma": 5.86
-          }
-      ]
-  }
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#0066ff"
+            },
+            {
+                "saturation": 74
+            },
+            {
+                "lightness": 100
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "weight": 0.6
+            },
+            {
+                "saturation": -85
+            },
+            {
+                "lightness": 61
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#5f94ff"
+            },
+            {
+                "lightness": 26
+            },
+            {
+                "gamma": 5.86
+            }
+        ]
+    }
 ];
-
 // Escapes HTML characters in a template literal string, to prevent XSS.
 // See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
 function sanitizeHTML(strings) {
@@ -136,15 +135,16 @@ function sanitizeHTML(strings) {
  * Initialize the Google Map.
  */
 function initMap() {
+  
   // Create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
+    zoom: 12,
     center: {lat:41.481674, lng: -81.826981},
     styles: mapStyle,
   });
 
   // Load the stores GeoJSON onto the map.
-  map.data.loadGeoJson('stores.json', {idPropertyName: 'storeid'});
+  map.data.loadGeoJson('stores.json', {idPropertyName: 'storeid'}, {idPropertyName: 'description'});
 
   // Define the custom marker icons, using the store's "category".
   map.data.setStyle((category) => {
@@ -155,7 +155,6 @@ function initMap() {
       },
     };
   });
-
   const apiKey = 'AIzaSyCsiAYBYa0EyqNZadx3OXXorQbTQvIDrQI';
   const infoWindow = new google.maps.InfoWindow();
 
@@ -168,10 +167,11 @@ function initMap() {
     const description = event.feature.getProperty('description');
     const position = event.feature.getGeometry().get();
     const content = sanitizeHTML`
-      <img style="float:left; width:200px; margin-top:30px" src="https://maps.googleapis.com/maps/api/streetview?size=350x250&location=${position.lat()},${position.lng()}&key=${apiKey}">
+      <img style="float:left; width:210px; margin-top:30px" src="https://maps.googleapis.com/maps/api/streetview?size=370x280&location=${position.lat()},${position.lng()}&key=${apiKey}">
       <div style="margin-left:220px; margin-bottom:20px;">
         <h2>${name}</h2><p>${description}</p>
         <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
+        <p><img style="float:right" src="https://github.com/brittanyyee/order_online_pickup_at_nearest_store/blob/master/buy_online_pickup_instore/Marcs_logo_2%20(2).png?raw=true"</p>
       </div>
       `;
 
@@ -194,7 +194,7 @@ function initMap() {
 
   card.setAttribute('id', 'pac-card');
   title.setAttribute('id', 'title');
-  title.textContent = 'Find the nearest store';
+  title.textContent = 'Find your nearest Marcs Store Location';
   titleBar.appendChild(title);
   container.setAttribute('id', 'pac-container');
   input.setAttribute('id', 'pac-input');
@@ -269,7 +269,6 @@ async function calculateDistances(data, origin) {
     stores.push(storeNum);
     destinations.push(storeLoc);
   });
-
   // Retrieve the distances of each store from the origin
   // The returned list will be in the same order as the destinations list
   const service = new google.maps.DistanceMatrixService();
@@ -280,20 +279,26 @@ async function calculateDistances(data, origin) {
           reject(response);
         } else {
           const distances = [];
+          const durations = [];
           const results = response.rows[0].elements;
           for (let j = 0; j < results.length; j++) {
             const element = results[j];
             const distanceText = element.distance.text;
             const distanceVal = element.distance.value;
+            const durationText = element.duration.text;
+            const durationVal = element.duration.value;
             const distanceObject = {
               storeid: stores[j],
               distanceText: distanceText,
               distanceVal: distanceVal,
+              durationText: durationText,
+              durationVal: durationVal
             };
             distances.push(distanceObject);
           }
 
           resolve(distances);
+          resolve(durations);
         }
       });
     });
@@ -304,20 +309,19 @@ async function calculateDistances(data, origin) {
     travelMode: 'DRIVING',
     unitSystem: google.maps.UnitSystem.IMPERIAL,
   });
-
   distancesList.sort((first, second) => {
-    return first.distanceVal - second.distanceVal;
+    return first.distanceVal - second.distanceVal 
+  
   });
-
   return distancesList;
+  
 }
-
 /**
  * Build the content of the side panel from the sorted list of stores
  * and display it.
  * @param {google.maps.Data} data The geospatial data object layer for the map
- * @param {object[]} stores An array of objects with a distanceText,
- * distanceVal, and storeid property.
+ * @param {object[]} stores An array of objects with a distanceText(created),
+ * distanceVal(created), and storeid property(geojson property).
  */
 function showStoresList(data, stores) {
   if (stores.length == 0) {
@@ -335,12 +339,10 @@ function showStoresList(data, stores) {
     }
     if (getNextPage) getNextPage();
   } else {
-    panel.setAttribute('id', 'panel');
+    panel.setAttribute('id', 'panel', 'description');
     const body = document.body;
     body.insertBefore(panel, body.childNodes[0]);
   }
-
-
   // Clear the previous details
   while (panel.lastChild) {
     panel.removeChild(panel.lastChild);
@@ -350,13 +352,20 @@ function showStoresList(data, stores) {
     // Add store details with text formatting
     const name = document.createElement('p');
     name.classList.add('place');
+
     const currentStore = data.getFeatureById(store.storeid);
     name.textContent = currentStore.getProperty('name');
     panel.appendChild(name);
+
     const distanceText = document.createElement('p');
     distanceText.classList.add('distanceText');
     distanceText.textContent = store.distanceText;
     panel.appendChild(distanceText);
+    
+    const durationText = document.createElement('p');
+    durationText.classList.add('distanceText');
+    durationText.textContent = store.durationText;
+    panel.appendChild(durationText);
   });
 
   // Open the panel
